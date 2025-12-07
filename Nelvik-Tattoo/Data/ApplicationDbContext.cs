e6e9bac57e9e6314470f62e0baa8c4c6ac252389
Martine Nelvik <manelvik@uia.no>
2025-12-07 15:12:38 +0100

    Testet opplasting av nytt bilde som ble lagt til i prosjektet i Gallery/

--- Nelvik-Tattoo/Data/ApplicationDbContext.cs
+++ Nelvik-Tattoo/Data/ApplicationDbContext.cs
@@ -15,4 +15,7 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 
     public DbSet<Booking> Bookings { get; set; }
     public DbSet<GalleryDesign> GalleryDesigns { get; set; }
+    
+    public DbSet<FlashDesign> FlashDesigns => Set<FlashDesign>();
+
 }
