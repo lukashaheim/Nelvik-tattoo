e4a9595ca10b555a99331625dce1933c62099f7b
Lukas Sopp Håheim <lukassh@uia.no>
2025-12-08 17:48:23 +0100

    possible to post photo

--- Nelvik-Tattoo/Controllers/BookingController.cs
+++ Nelvik-Tattoo/Controllers/BookingController.cs
@@ -6,22 +6,43 @@ namespace Nelvik_Tattoo.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly IWebHostEnvironment _env;
         private readonly ApplicationDbContext _db;
 
-        public BookingController(ApplicationDbContext db)
+        public BookingController(IWebHostEnvironment env, ApplicationDbContext db)
         {
+            _env = env;
             _db = db;
         }
 
         [HttpGet]
-        public IActionResult Index()
-        {
-            return View(new Booking());
-        }
+        public IActionResult Index() => View(new Booking());
 
         [HttpPost]
-        public async Task<IActionResult> Index([FromForm] Booking model)
+        public async Task<IActionResult> Index(Booking model)
         {
+            if (!ModelState.IsValid)
+                return View(model);
+            
+            if (model.Photo is { Length: > 0 })
+            {
+                var uploadsRoot = Path.Combine(
+                    _env.WebRootPath,
+                    "Images", "Booking", "Uploads");
+
+                Directory.CreateDirectory(uploadsRoot);
+
+                var fileName = $"{Guid.NewGuid()}{Path.GetExtension(model.Photo.FileName)}";
+                var filePath = Path.Combine(uploadsRoot, fileName);
+
+                await using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await model.Photo.CopyToAsync(stream);
+                }
+                
+                model.ImagePath = $"/Images/Booking/Uploads/{fileName}";
+            }
+            
             _db.Bookings.Add(model);
             await _db.SaveChangesAsync();
 
@@ -29,9 +50,6 @@ namespace Nelvik_Tattoo.Controllers
         }
 
         [HttpGet]
-        public IActionResult Confirmation()
-        {
-            return View();
-        }
+        public IActionResult Confirmation() => View();
     }
 }
