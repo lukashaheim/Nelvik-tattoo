04487ed49bc252312c40ec225acf57b6c4adb897
Lukas Sopp Håheim <lukassh@uia.no>
2025-12-08 17:37:08 +0100

    fixed backend. possible to send requests

--- Nelvik-Tattoo/Controllers/BookingController.cs
+++ Nelvik-Tattoo/Controllers/BookingController.cs
@@ -1,25 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using Nelvik_Tattoo.Data;
 using Nelvik_Tattoo.Models;
 
-namespace Nelvik_Tattoo.Controllers;
-
-public class BookingController : Controller
+namespace Nelvik_Tattoo.Controllers
 {
-    private readonly IWebHostEnvironment _env;
-    public BookingController(IWebHostEnvironment env) => _env = env;
-
-    [HttpGet]
-    public IActionResult Index() => View(new Booking());
-    
-    [HttpGet]
-    public IActionResult Confirmation() => View();
-    
-    [HttpPost]
-    public async Task<IActionResult> Index(Booking model)
+    public class BookingController : Controller
     {
-        if (!ModelState.IsValid)
-            return View(model);
-        
-        return RedirectToAction(nameof(Confirmation));
+        private readonly ApplicationDbContext _db;
+
+        public BookingController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Booking());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([FromForm] Booking model)
+        {
+            _db.Bookings.Add(model);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Confirmation));
+        }
+
+        [HttpGet]
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
     }
 }
